5e4d14b83c51f92a2edfe37461f77e9845cc8622
Christian Gonzalez <christian.gonzalez@garsalabs.com>
2024-10-01 01:01:10 -0600

    fix endpoints

--- app/src/main/java/com/sesi/chris/animangaquiz/data/api/retrofit/QuizRetrofitService.java
+++ app/src/main/java/com/sesi/chris/animangaquiz/data/api/retrofit/QuizRetrofitService.java
@@ -39,68 +39,53 @@ public interface QuizRetrofitService {
     Observable<List<Anime>> getAllAnimes(@Body Credentials request);
 
     @POST(Constants.EndPoint.GET_ALL_ANIMES_IMG)
-    @FormUrlEncoded
     Observable<List<Anime>> getAllAnimesImg(@Body Credentials request);
 
     @POST(Constants.EndPoint.GET_ALL_ANIMES_FOR_WALLPAPER)
-    @FormUrlEncoded
     Observable<List<Anime>> getAllanimesForWallpaper(@Body Credentials request);
 
     @POST(Constants.EndPoint.GET_QUESTIONS_BY_ANIME_AND_LEVEL)
-    @FormUrlEncoded
     Observable<List<Preguntas>> getQuestionsByAnimeAndLevel(@Body QuestionByAnimeLevelRequest request);
 
     @POST(Constants.EndPoint.CHECK_LEVEL_AND_SCORE_BY_ANIME_AND_USER)
-    @FormUrlEncoded
     Observable<ScoreResponse> checkLevelAndScore(@Body CheckLevelAndScoreRequest request);
 
     @POST(Constants.EndPoint.UPDATE_LEVEL_AND_SCORE)
-    @FormUrlEncoded
     Observable<UpdateResponseD> updateLevelScoreGemsTotalScore(@Body UpdateLevelScoreGemsTotalScoreRequest request);
 
     @POST(Constants.EndPoint.GET_WALLPAPER_BY_ANIME)
-    @FormUrlEncoded
     Observable<List<Wallpaper>> getWallpaperByAnime(@Body GetWallpaperByAnimeRequest request);
 
     @POST(Constants.EndPoint.UPDATE_GEMAS)
-    @FormUrlEncoded
     Observable<UpdateResponseD> updateGemas(@Body UpdateGemasRequest request);
 
     @POST(Constants.EndPoint.REGISTRO_NUEVO_USUARIO)
     Observable<UpdateResponseD> registroNuevoUsuario(@Body NewUserRequest request);
 
     @POST(Constants.EndPoint.SEARCH_FRIEND_BY_USER_NAME)
-    @FormUrlEncoded
     Observable<List<User>> searchFriendByUserName(@Body SearchFriendByUserNameRequest request);
 
     @POST(Constants.EndPoint.ADD_FRIEND_BY_ID)
-    @FormUrlEncoded
     Observable<UpdateResponseD> addFriendById(@Body AddFriendByIdRequest request);
 
     @POST(Constants.EndPoint.GET_AVATARS_BY_ANIME)
-    @FormUrlEncoded
     Observable<List<Wallpaper>> getAvatarsByAnime(@Body GetAvatarsByAnimeRequest request);
 
     @POST(Constants.EndPoint.GET_ALL_FRIENDS_BY_USER)
-    @FormUrlEncoded
     Observable<List<User>> getAllFriendsByUser(@Body Credentials request);
 
     @POST(Constants.EndPoint.UPDATE_AVATAR)
-    @FormUrlEncoded
     Observable<UpdateResponseD> updateAvatar(@Body UpdateAvatarRequest request);
 
     @POST(Constants.EndPoint.CHECK_USER_FACEBOOK)
-    @FormUrlEncoded
     Observable<LoginResponse> validaUsuarioFacebook(@Field(Constants.ParametersBackEnd.USER_NAME) String username);
 
     @POST(Constants.EndPoint.UPDATE_ESFERAS)
-    @FormUrlEncoded
     Observable<UpdateResponseD> updateEsferas(@Body UpdateEsferasRequest request);
 
     @POST(Constants.EndPoint.GET_QUESTIONS_BY_ANIME_IMG)
-    @FormUrlEncoded
     Observable<List<Preguntas>> getQuestionsByAnimeImg(@Body GetQuestionsByAnimeImgRequest request);
+
     @POST(Constants.EndPoint.DELETE_USER)
-    @FormUrlEncoded
     Observable<UpdateResponseD> deleteUser(@Body DeleteUserRequest request);
     }
